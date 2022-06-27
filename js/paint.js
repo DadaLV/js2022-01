4411540d1298701994ced05bf924942a0bb746b9
DadaLV <vynohradova.l.a@gmail.com>
2022-06-27 19:50:40 +0300

    formula in paint changed

--- js/paint.js
+++ js/paint.js
@@ -4,7 +4,7 @@ function paint() {
   let w = document.getElementById("width").value;
 
   bottles =
-    (parseInt(h) * parseInt(l) * 2 + parseInt(h) * parseInt(w) * 2) / 16;
+    (parseInt(l) +  parseInt(w)) * parseInt(h) * 2 / 16;
   console.log(bottles);
   document.getElementById("amount").innerHTML = bottles;
 }

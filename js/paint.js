111d02d41cde68ab3f1e1ffef49339a478e2b107
DadaLV <vynohradova.l.a@gmail.com>
2022-06-27 19:41:29 +0300

    paint js added

---
+++ js/paint.js
@@ -0,0 +1,9 @@
+function paint() {
+  let h = document.getElementById("hight").value;
+  let l = document.getElementById("length").value;
+  let w = document.getElementById("width").value;
+  
+  bottles = ((parseInt(h) * parseInt(l)*2) + (parseInt(h) * parseInt(w)*2))  / 16;
+  console.log(bottles)
+  document.getElementById("amount").innerHTML = bottles;
+}

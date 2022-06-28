e10b0b892e51e51b90fe3e0a90365c5c4123cf29
DadaLV <vynohradova.l.a@gmail.com>
2022-06-28 22:26:22 +0300

    mistakes in ticket was corrected

--- js/hw2/happy-ticket.js
+++ js/hw2/happy-ticket.js
@@ -1,8 +1,8 @@
 function happyTicket() {
   let ticketNumber = document.getElementById("ticket").value.split("");
   let tickeNumber = Array.from(ticketNumber);
-  let firstThreeNumber = tickeNumber[0] + tickeNumber[1] + tickeNumber[2];
-  let secondThreeNumber = tickeNumber[3] + tickeNumber[4] + tickeNumber[5];
+  let firstThreeNumber = parseInt(tickeNumber[0]) + parseInt(tickeNumber[1]) + parseInt(tickeNumber[2]);
+  let secondThreeNumber = parseInt(tickeNumber[3]) + parseInt(tickeNumber[4]) + parseInt(tickeNumber[5]);
   let text4;
   if (firstThreeNumber == secondThreeNumber) {
     text4 = "You have Happy ticket";
@@ -11,4 +11,4 @@ function happyTicket() {
   }
   document.getElementById("showtick").innerHTML = text4;
 }
-console.log(happyTicket(111111));
+

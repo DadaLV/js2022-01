f9c4d8144b59d1f96009cb41d03b1edd380cc04b
DadaLV <vynohradova.l.a@gmail.com>
2022-07-15 02:45:05 +0300

    morse was added

---
+++ js/hw5/calc.js
@@ -0,0 +1,8 @@
+function Calculator(a, b, sign) {
+  let result = eval(a + sign + b);
+  return result % 1 === 0 ? result : result.toFixed(1);
+}
+
+console.log(Calculator(12, 4, '+'));
+console.log(Calculator(18, 0, '/'));
+

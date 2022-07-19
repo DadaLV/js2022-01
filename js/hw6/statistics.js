15817b7450477905407105b2927ce965f38e68d6
DadaLV <vynohradova.l.a@gmail.com>
2022-07-19 23:41:51 +0300

    statistic was changed

--- js/hw6/statistics.js
+++ js/hw6/statistics.js
@@ -1,7 +1,20 @@
 function statistics(str) {
-  let letters = str.match(/[a-zA-Z]/g).length;
-  let digit = str.match(/[0-9]/g).length;
-  let symbol = str.match(/[^A-Za-z0-9]/g).length;
+  let letters, symbol, digit;
+  if (str.match(/[a-zA-Z]/g) !== null) {
+    letters = str.match(/[a-zA-Z]/g).length;
+  } else {
+    letters = "0";
+  }
+  if (str.match(/[0-9]/g) !== null) {
+    digit = str.match(/[0-9]/g).length;
+  } else {
+    digit = "0";
+  }
+  if (str.match(/[^A-Za-z0-9]/g) !== null) {
+    symbol = str.match(/[^A-Za-z0-9]/g).length;
+  } else {
+    symbol = "0";
+  }
   return (
     "We have " +
     letters +
@@ -13,3 +26,4 @@ function statistics(str) {
   );
 }
 console.log(statistics("AFjjk12lEE,++.66"));
+console.log(statistics("AFjjk12lEE66"));
